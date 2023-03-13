aa7a1399de50aa19f312ae71b34237776b1f5d73
Ozerskay <olesja.94@mail.ru>
2023-03-13 20:58:44 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,23 +1,49 @@
 function getArrayParams(...arr) {
-
+let min =  Infinity;
+let max = -Infinity;
+let sum = min + max;
+for(i=0, i > max) {
+  i = max;
+  if(i=0, i < min)
+  i = min;
+  console.log(i + sum)
+}
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+let sum = min + max;
+let min = 5;
+let max = 10;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  Math.max(10);
+  Math.min(5);
+  return Math.abs(10 - 5);
 }
 
 function differenceEvenOddWorker(...arr) {
-
-}
+let sumEvenElement = 0;
+let sumOddElement = 0;
+let element;
+for (let i > 0; i++) {
+if (element % 2) 
+  sumEvenElemen ++;
+} else (element != % 2) {
+sumOddElement ++;
+ }
+return Math.abs(sumEvenElement,sumOddElement);
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let countOddElement = 0;
+  let element;
+  for (let i > 0; i++) {
+  if (element % 2)
+  sumEvenElement + i && countOddElement ++;
 }
+return(sumEvenElement/countOddElement);
 
 function makeWork (arrOfArr, func) {
 
